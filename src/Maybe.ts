b880fe6a4ef6635c3590947e2fb7a635a507f310
gigobyte <gigobest2@gmail.com>
2018-02-06 23:35:56 +0200

    Added more maybe methods

--- src/Maybe.ts
+++ src/Maybe.ts
@@ -1,6 +1,7 @@
 export type Nothing = void | null | undefined
 export type Just<T> = T
 export type Maybe<T> = Just<T> | Nothing
+export type MaybePatterns<T, U> = {Just: (value: T) => U, Nothing: () => U}
 
 export const Just = <T>(value: T): Maybe<T> =>
     value
@@ -8,3 +9,20 @@ export const Just = <T>(value: T): Maybe<T> =>
 export const Nothing =
     null
 
+export const isNothing = <T>(maybe: Maybe<T>): maybe is Nothing =>
+    maybe == null
+
+export const isJust = <T>(maybe: Maybe<T>): maybe is Just<T> =>
+    maybe != null
+
+export const map = <T, U>(mapper: (value: T) => U, maybe: Maybe<T>): Maybe<U> =>
+    isNothing(maybe) ? Nothing : Just(mapper(maybe))
+
+export const chain = <T, U>(mapper: (value: T) => Maybe<U>, maybe: Maybe<T>): Maybe<U> =>
+    isNothing(maybe) ? Nothing : mapper(maybe)
+
+export const caseOf = <T, U>(maybe: Maybe<T>, patterns: MaybePatterns<T, U>): Maybe<U> =>
+    isNothing(maybe) ? patterns.Nothing() : patterns.Just(maybe)
+
+export const from = <T>(nullableValue: T): Maybe<T> =>
+    nullableValue == null ? Nothing : Just(nullableValue)
