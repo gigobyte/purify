f0ee5ac97c291611eb6195bd71e1f3e6aad8b63b
gigobyte <gigobest2@gmail.com>
2018-02-07 21:08:31 +0200

    Added lots of maybe methods

--- src/Maybe.ts
+++ src/Maybe.ts
@@ -18,11 +18,38 @@ export const isJust = <T>(maybe: Maybe<T>): maybe is Just<T> =>
 export const map = <T, U>(mapper: (value: T) => U, maybe: Maybe<T>): Maybe<U> =>
     isNothing(maybe) ? Nothing : Just(mapper(maybe))
 
+export const map2 = <T, U, I>(mapper: (a: T, b: U) => I, maybeA: Maybe<T>, maybeB: Maybe<U>): Maybe<I> =>
+    (isNothing(maybeA) || isNothing(maybeB)) ? Nothing : Just(mapper(maybeA, maybeB))
+
 export const chain = <T, U>(mapper: (value: T) => Maybe<U>, maybe: Maybe<T>): Maybe<U> =>
     isNothing(maybe) ? Nothing : mapper(maybe)
 
 export const caseOf = <T, U>(maybe: Maybe<T>, patterns: MaybePatterns<T, U>): Maybe<U> =>
     isNothing(maybe) ? patterns.Nothing() : patterns.Just(maybe)
 
-export const from = <T>(nullableValue: T): Maybe<T> =>
+export const of = <T>(nullableValue: T): Maybe<T> =>
     nullableValue == null ? Nothing : Just(nullableValue)
+
+export const alt = <T>(maybe1: Maybe<T>, maybe2: Maybe<T>): Maybe<T> =>
+    isNothing(maybe1) ? isNothing(maybe2) ? Nothing : maybe2 : maybe1
+
+export const ap = <T, U>(maybeF: Maybe<(value: T) => U>, maybe: Maybe<T>): Maybe<U> =>
+    isNothing(maybeF) ? Nothing : map(maybeF, maybe)
+
+export const withDefault = <T>(defaultValue: T, maybe: Maybe<T>): T =>
+    isNothing(maybe) ? defaultValue : maybe
+
+export const lift = <T, U>(mapper: (value: T) => U) => (maybe: Maybe<T>): Maybe<U> =>
+    map(mapper, maybe)
+
+export const liftM = <T, U>(mapper: (value: T) => Maybe<U>) => (maybe: Maybe<T>): Maybe<U> =>
+    chain(mapper, maybe)
+
+export const toList = <T>(maybe: Maybe<T>): never[] | [T] =>
+    isNothing(maybe) ? [] : [maybe]
+
+export const catMaybes = <T>(list: Maybe<T>[]): T[] =>
+    list.filter(isJust)
+
+export const mapMaybe = <T, U>(mapper: (value: T) => Maybe<U>, list: T[]): U[] =>
+    catMaybes(list.map(mapper))
