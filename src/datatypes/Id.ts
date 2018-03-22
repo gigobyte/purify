35de2775d3ca284fc2e2d5b346f595e7ff8ae081
gigobyte <gigobest2@gmail.com>
2018-03-22 22:55:12 +0200

    Id draft

---
+++ src/datatypes/Id.ts
@@ -0,0 +1,45 @@
+import Show from '../typeclasses/Show'
+import Functor from '../typeclasses/Functor'
+import Chain from '../typeclasses/Chain'
+import Apply from '../typeclasses/Apply'
+
+export interface Id<T> extends Id_<T> { }
+
+export class Id_<T> implements Show, Functor<T>, Chain<T>, Apply<T> {
+    constructor(private readonly value: T) {}
+
+    readonly 'fantasy-land/map' = this.map
+    readonly 'fantasy-land/chain' = this.chain
+    readonly 'fantasy-land/ap' = this.ap
+
+    unwrap(): T {
+        return this.value
+    }
+
+    toJSON(): T {
+        return this.value
+    }
+
+    inspect(): string {
+        return this.value != null ? this.value.toString() : JSON.stringify(this.value)
+    }
+
+    toString(): string {
+        return this.inspect()
+    }
+
+    map<U>(f: (value: T) => U): Id<U> {
+        return Id(f(this.value))
+    }
+
+    chain<U>(f: (value: T) => Id<U>): Id<U> {
+        return f(this.value)
+    }
+
+    ap<U>(f: Id<(value: T) => U>): Id<U> {
+        return Id(f.value(this.value))
+    }
+}
+
+export const Id = <T>(value: T): Id<T> =>
+    new Id_(value)
