cba5aab42fb14cd97fe0ebfb49a3d96616c39447
gigobyte <gigobest2@gmail.com>
2018-04-12 19:44:12 +0300

    Gatsby setup

--- site/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './App'
-
-document.onreadystatechange = function () {
-    if(document.readyState === 'complete') {
-        ReactDOM.render(<App />, document.querySelector('.app'))
-    }
-}
