324f78d15377908341485c25cc35b2e4b58bcb1a
gigobyte <gigobest2@gmail.com>
2018-04-13 23:20:55 +0300

    Added all Maybe methods to docs

--- site/src/data.js
+++ site/src/data.js
@@ -120,6 +120,15 @@ const data = {
                     ]
                 },
                 {
+                    name: 'caseOf',
+                    signatureTS: '<U>(patterns: {Just: (value: T) => U, Nothing: () => U}): U',
+                    description: 'Structural pattern matching for `Maybe` in the form of a function',
+                    examples: [
+                        {input: 'Just(5).caseOf({ Just: x => x + 1, Nothing: () => 0 })', output: '6'},
+                        {input: 'Nothing.caseOf({ Just: x => x + 1, Nothing: () => 0 })', output: '0'}
+                    ]
+                },
+                {
                     name: 'equals',
                     signatureML: 'Maybe a ~> Maybe a -⁠> Bool',
                     signatureTS: '(other: Maybe<T>): boolean',
@@ -176,6 +185,125 @@ const data = {
                         {input: 'Just(5).ap(Nothing)', output: 'Nothing'},
                         {input: 'Nothing.ap(Just(x => x + 1))', output: 'Nothing'},
                         {input: 'Nothing.ap(Nothing)', output: 'Nothing'}
+                    ]
+                },
+                {
+                    name: 'alt',
+                    signatureML: 'Maybe a ~> Maybe a -⁠> Maybe a',
+                    signatureTS: '(other: Maybe<T>): Maybe<T>',
+                    description: 'Returns the first `Just` between `this` and another `Maybe` or `Nothing` if both `this` and the argument are `Nothing`',
+                    examples: [
+                        {input: 'Just(5).alt(Just(6))', output: 'Just(5)'},
+                        {input: 'Just(5).alt(Nothing)', output: 'Just(5)'},
+                        {input: 'Nothing.alt(Just(5))', output: 'Just(5)'},
+                        {input: 'Nothing.alt(Nothing)', output: 'Nothing'}
+                    ]
+                },
+                {
+                    name: 'chain',
+                    signatureML: 'Maybe a ~> (a -⁠> Maybe b) -⁠> Maybe b',
+                    signatureTS: '<U>(f: (value: T) => Maybe<U>): Maybe<U>',
+                    description: 'Transforms `this` with a function that returns a `Maybe`. Useful for chaining many computations that may fail',
+                    examples: [
+                        {input: 'Just(5).chain(x => Just(x + 1))', output: 'Just(6)'},
+                        {input: 'Nothing.chain(x => Just(x + 1))', output: 'Nothing'}
+                    ]
+                },
+                {
+                    name: 'reduce',
+                    signatureML: 'Maybe a ~> ((b, a) -⁠> b, b) -⁠> b',
+                    signatureTS: '<U>(reducer: (accumulator: U, value: T) => U, initialValue: U): U',
+                    description: 'Takes a reducer and a initial value and returns the initial value if `this` is `Nothing` or the result of applying the function to the initial value and the value inside `this`',
+                    examples: [
+                        {input: 'Just(5).reduce((acc, x) => x * acc, 2)', output: '10'},
+                        {input: 'Nothing.reduce((acc, x) => x * acc, 0)', output: '0'},
+                    ]
+                },
+                {
+                    name: 'extend',
+                    signatureML: 'Maybe a ~> (Maybe a -⁠> b) -⁠> Maybe b',
+                    signatureTS: '<U>(f: (value: Maybe<T>) => U): Maybe<U>',
+                    description: 'Return `this` if it\'s `Nothing`, otherwise it returns the result of applying the function argument to `this` and wrapping it in a `Just`',
+                    examples: [
+                        {input: 'Just(5).extend(x => x.isJust())', output: 'Just(true)'},
+                        {input: 'Nothing.extend(x => x.isJust())', output: 'Nothing'}
+                    ]
+                },
+                {
+                    name: 'unsafeCoerce',
+                    signatureML: 'Maybe a ~> a ',
+                    signatureTS: '(): T',
+                    description: 'Returns the value inside `this` or throws an error if `this` is `Nothing`',
+                    examples: [
+                        {input: 'Just(5).unsafeCoerce()', output: '5'},
+                        {input: 'Nothing.unsafeCoerce()', output: '// Error: Maybe got coerced to a null'}
+                    ]
+                },
+                {
+                    name: 'orDefault',
+                    signatureML: 'Maybe a ~ a -> a',
+                    signatureTS: '(defaultValue: T): T',
+                    description: 'Returns the default value if `this` is `Nothing`, otherwise it unwraps `this` and returns the value',
+                    examples: [
+                        {input: 'Just(5).orDefault(0)', output: '5'},
+                        {input: 'Nothing.orDefault(0)', output: '0'}
+                    ]
+                },
+                {
+                    name: 'mapOrDefault',
+                    signatureML: 'Maybe a ~> (a -> b) -> b -> b',
+                    signatureTS: '<U>(f: (value: T) => U, defaultValue: U): U',
+                    description: 'Maps over `this` and returns the resulting value or returns the default value if `this` is `Nothing`',
+                    examples: [
+                        {input: 'Just(5).mapOrDefault(x => x + 1, 0)', output: '6'},
+                        {input: 'Nothing.mapOrDefault(x => x + 1, 0)', output: '0'}
+                    ]
+                },
+                {
+                    name: 'extract',
+                    signatureTS: '(): T | null',
+                    description: 'Returns the value inside `this` or null if `this` is `Nothing`',
+                    examples: [
+                        {input: 'Just(5).extract()', output: '5'},
+                        {input: 'Nothing.extract()', output: 'null'}
+                    ]
+                },
+                {
+                    name: 'toList',
+                    signatureML: 'Maybe a ~> [a]',
+                    signatureTS: '(): T[]',
+                    description: 'Returns empty list if the `Maybe` is `Nothing` or a list where the only element is the value of `Just`',
+                    examples: [
+                        {input: 'Just(5).toList()', output: '[5]'},
+                        {input: 'Nothing.toList()', output: '[]'}
+                    ]
+                },
+                {
+                    name: 'toEither',
+                    signatureML: 'Maybe a ~> e -> Either e a',
+                    signatureTS: '<L>(left: L): Either<L, T>',
+                    description: 'Constructs a `Right` from a `Just` or a `Left` with a provided left value if `this` is `Nothing`',
+                    examples: [
+                        {input: `Just(5).toEither('Error')`, output: 'Right(5)'},
+                        {input: `Nothing.toEither('Error')`, output: `Left('Error')`}
+                    ]
+                },
+                {
+                    name: 'ifJust',
+                    signatureTS: '(effect: (value: T) => any): this',
+                    description: 'Runs an effect if `this` is `Just`, returns `this` for easier composiblity',
+                    examples: [
+                        {input: `Just(5).ifJust(() => console.log('success'))`, output: `// success`},
+                        {input: `Nothing.ifJust(() => console.log('success'))`, output: ''}
+                    ]
+                },
+                {
+                    name: 'ifNothing',
+                    signatureTS: '(effect: (value: T) => any): this',
+                    description: 'Runs an effect if `this` is `Nothing`, returns `this` for easier composiblity',
+                    examples: [
+                        {input: `Just(5).ifNothing(() => console.log('failure'))`, output: ''},
+                        {input: `Nothing.ifNothing(() => console.log('failure'))`, output: '// failure'}
                     ]
                 }
             ]
