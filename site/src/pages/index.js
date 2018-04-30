6543acfdeff247a01830020bcc7763d8a8fa447f
gigobyte <gigobest2@gmail.com>
2018-04-30 21:28:14 +0300

    Landing page concept

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -19,8 +19,8 @@ const Subtitle = styled.h2`
 
 const NavBar = styled.div`
     background-color: #007acc;
-    height: 65px;
-    min-height: 65px;
+    height: 60px;
+    min-height: 60px;
     display: flex;
     justify-content: flex-end;
     padding-right: 80px;
@@ -58,6 +58,17 @@ const InstallBox = styled.div`
     border: 1px dashed #007acc;
 `
 
+const FeaturesContainer = styled.div`
+    display: flex;
+`
+
+const Feature = styled.div`
+    flex: 1;
+`
+
+const FeatureTitle = styled.h3`
+`
+
 const IndexPage = () =>
     <Container>
         <NavBar>
@@ -72,6 +83,22 @@ const IndexPage = () =>
                      $ npm install pure-ts
                 </InstallBox>
             </Heading>
+            <FeaturesContainer>
+                <Feature />
+                <Feature>
+                    <FeatureTitle>Algebraic Data Types</FeatureTitle>
+                    Easily separate conditional logic and error handling with ADTs like Maybe and Either
+                </Feature>
+                <Feature>
+                    <FeatureTitle>Typeclasses</FeatureTitle>
+                    Implement instances of common typeclasses like Functor and Monad for your own data structures
+                </Feature>
+                <Feature>
+                    <FeatureTitle>Utility functions</FeatureTitle>
+                    Avoid code duplication by making use of pure's utility functions and combinators
+                </Feature>
+                <Feature />
+            </FeaturesContainer>
         </Content>
     </Container>
 
