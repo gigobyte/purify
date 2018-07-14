6487d9df13b24f80c1483c5b6d2188320fdc3ebc
gigobyte <gigobest2@gmail.com>
2018-07-14 21:59:47 +0300

    Adding favicon

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -3,7 +3,8 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-typescript',
-    {resolve: 'gatsby-plugin-google-fonts', options: { fonts: ['Titillium Web'] }}
+    {resolve: 'gatsby-plugin-google-fonts', options: { fonts: ['Titillium Web'] }},
+    {resolve: 'gatsby-plugin-favicon', options: { logo: './images/favicon.png', injectHTML: true, icons: { android: true, appleIcon: true, appleStartup: true } }}
   ],
   pathPrefix: '/pure'
 }
