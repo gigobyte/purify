046d573776ff797d80c073626d1c4f1581514781
gigobyte <gigobest2@gmail.com>
2018-08-29 17:45:02 +0300

    Simplified concat implementation

--- src/utils/concat.ts
+++ src/utils/concat.ts
@@ -4,7 +4,6 @@ function concat<T>(a: T, b: T): T
 function concat<T>(a: T[], b: T[]): T[]
 function concat(a: number, b: number): number
 function concat(a: string, b: string): string
-function concat(a: boolean, b: boolean): boolean
 function concat<T extends Semigroup<any>>(a: T, b: T): T
 function concat(a: any, b: any): any {
     if (typeof a.concat === 'function' && typeof b.concat === 'function') {
@@ -13,14 +12,6 @@ function concat(a: any, b: any): any {
 
     if (typeof a === 'number' && typeof b === 'number') {
         return a + b
-    }
-
-    if (typeof a === 'string' && typeof b === 'string') {
-        return a + b
-    }
-
-    if (typeof a === 'boolean' && typeof b === 'boolean') {
-        return a || b
     }
 }
 
