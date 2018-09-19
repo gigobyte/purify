9e5497d8d19963a47c5f486844be818091c887ea
gigobyte <gigobest2@gmail.com>
2018-09-19 20:38:03 +0300

    Improvements to `State`

--- src/adts/State.ts
+++ src/adts/State.ts
@@ -11,19 +11,20 @@ import { Applicative } from '../typeclasses/Applicative'
 import { Chain } from '../typeclasses/Chain'
 import { Monad } from '../typeclasses/Monad'
 
-export type StateFn<S, A> = (state: S) => Tuple<A, S>
+export type StateFn<S, R> = (state: S) => Tuple<R, S>
 
-export interface State<S, A> extends State_<S, A> { }
+export interface State<S, R> extends State_<S, R> { }
 
 export interface IState {
-    <S, A>(f: (state: S) => Tuple<A, S>): State<S, A>
+    <S, R>(f: (state: S) => Tuple<R, S>): State<S, R>
+    of: typeof State_.of
     get: typeof State_.get
     put: typeof State_.put
     modify: typeof State_.modify
 }
 
-class State_<S, A> implements Functor<A>, Apply<A>, Applicative<A>, Chain<A>, Monad<A>, Show {
-    constructor(private readonly runner: StateFn<S, A>) {}
+class State_<S, R> implements Functor<R>, Apply<R>, Applicative<R>, Chain<R>, Monad<R>, Show {
+    constructor(private readonly run: StateFn<S, R>) {}
 
     readonly of = State_.of
     readonly 'fantasy-land/of' = this.of
@@ -31,12 +32,12 @@ class State_<S, A> implements Functor<A>, Apply<A>, Applicative<A>, Chain<A>, Mo
     readonly 'fantasy-land/ap' = this.ap
     readonly 'fantasy-land/chain' = this.chain
 
-    static of<S, A>(value: A): State<S, A> {
-        return State(s => Tuple(value, s))
+    static of<S = unknown, R = unknown>(result: R): State<S, R> {
+        return State(state => Tuple(result, state))
     }
 
     static get<S>(): State<S, S> {
-        return State(s => Tuple(s, s))
+        return State(state => Tuple(state, state))
     }
 
     static put<S>(state: S): State<S, unknown> {
@@ -44,50 +45,50 @@ class State_<S, A> implements Functor<A>, Apply<A>, Applicative<A>, Chain<A>, Mo
     }
 
     static modify<S>(f: (state: S) => S): State<S, unknown> {
-        return State(s => Tuple(undefined, f(s)))
+        return State(state => Tuple(undefined, f(state)))
     }
 
     inspect(): string {
-        return `State(${this.runner})`
+        return `State(${this.run})`
     }
 
     toString(): string {
         return this.inspect()
     }
 
-    toJSON(): StateFn<S, A> {
-        return this.runner
+    toJSON(): StateFn<S, R> {
+        return this.run
     }
 
-    runWith(state: S): Tuple<A, S> {
-        return this.runner(state)
+    runWith(state: S): Tuple<R, S> {
+        return this.run(state)
     }
 
-    evalWith(state: S): A {
-        return this.runner(state).fst()
+    evalWith(state: S): R {
+        return this.run(state).fst()
     }
 
     execWith(state: S): S {
-        return this.runner(state).snd()
+        return this.run(state).snd()
     }
 
-    map<B>(f: (value: A) => B): State<S, B> {
-        return State((s: S) => this.runWith(s).mapFirst(f))
+    map<B>(f: (result: R) => B): State<S, B> {
+        return State((state: S) => this.runWith(state).mapFirst(f))
     }
 
-    ap<B>(fstate: State<S, (value: A) => B>): State<S, B> {
+    ap<B>(fstate: State<S, (result: R) => B>): State<S, B> {
         return fstate.chain(f => this.map(f))
     }
 
-    chain<B>(f: (value: A) => State<S, B>): State<S, B> {
-        return State(s => {
-            const [a, s1] = this.runner(s)
-            return f(a).runWith(s1)
+    chain<B>(f: (result: R) => State<S, B>): State<S, B> {
+        return State(state => {
+            const [ newResult, newState ] = this.run(state)
+            return f(newResult).runWith(newState)
         })
     }
 }
 
-const StateConstructor = <S, A>(f: StateFn<S, A>): State<S, A> =>
+const StateConstructor = <S = unknown, R = unknown>(f: StateFn<S, R>): State<S, R> =>
     new State_(f)
 
-export const State: IState = Object.assign(StateConstructor, {get: State_.get, put: State_.put, modify: State_.modify})
+export const State: IState = Object.assign(StateConstructor, {get: State_.get, put: State_.put, modify: State_.modify, of: State_.of})
