819ae42f82066e169d5bdb3a2bab482fb9f5bbff
gigobyte <gigobest2@gmail.com>
2019-01-24 00:56:13 +0200

    Add docstrings

--- src/Either.ts
+++ src/Either.ts
@@ -5,40 +5,73 @@ export type EitherPatterns<L, R, T> =
   | { _: () => T }
 
 export interface Either<L, R> {
+  /** Internal property and subject to breaking changes, please use some of the available methods on the object if you want to access it */
   __value: L | R
+  /** Returns true if `this` is `Left`, otherwise it returns false */
   isLeft(): this is Either<L, never>
+  /** Returns true if `this` is `Right`, otherwise it returns false */
   isRight(): this is Either<never, R>
   toJSON(): L | R
   inspect(): string
   toString(): string
+  /** Given two functions, maps the value inside `this` using the first if `this` is `Left` or using the second one if `this` is `Right`.
+   * If both functions return the same type consider using `Either#either` instead
+   */
   bimap<L2, R2>(f: (value: L) => L2, g: (value: R) => R2): Either<L2, R2>
+  /** Maps the `Right` value of `this`, acts like an identity if `this` is `Left` */
   map<R2>(f: (value: R) => R2): Either<L, R2>
+  /** Maps the `Left` value of `this`, acts like an identity if `this` is `Right` */
   mapLeft<L2>(f: (value: L) => L2): Either<L2, R>
+  /** Applies a `Right` function over a `Right` value. Returns `Left` if either `this` or the function are `Left` */
   ap<R2>(other: Either<L, (value: R) => R2>): Either<L, R2>
+  /** Compares `this` to another `Either`, returns false if the constructors or the values inside are different, e.g. `Right(5).equals(Left(5))` is false */
   equals(other: Either<L, R>): boolean
+  /** Transforms `this` with a function that returns an `Either`. Useful for chaining many computations that may fail */
   chain<R2>(f: (value: R) => Either<L, R2>): Either<L, R2>
+  /** Flattens nested Eithers. `e.join()` is equivalent to `e.chain(x => x)` */
   join<R2>(this: Either<L, Either<L, R2>>): Either<L, R2>
+  /** Returns the first `Right` between `this` and another `Either` or the `Left` in the argument if both `this` and the argument are `Left` */
   alt(other: Either<L, R>): Either<L, R>
+  /** Takes a reducer and a initial value and returns the initial value if `this` is `Left` or the result of applying the function to the initial value and the value inside `this` */
   reduce<T>(reducer: (accumulator: T, value: R) => T, initialValue: T): T
+  /** Returns `this` if it\'s a `Left`, otherwise it returns the result of applying the function argument to `this` and wrapping it in a `Right` */
   extend<R2>(f: (value: Either<L, R>) => R2): Either<L, R2>
+  /** Returns the value inside `this` or throws an error if `this` is a `Left` */
   unsafeCoerce(): R
+  /** Structural pattern matching for `Either` in the form of a function */
   caseOf<T>(patterns: EitherPatterns<L, R, T>): T
+  /** Returns the value inside `this` if it\'s `Left` or a default value if `this` is `Right` */
   leftOrDefault(defaultValue: L): L
+  /** Returns the value inside `this` if it\'s `Right` or a default value if `this` is `Left` */
   orDefault(defaultValue: R): R
+  /** Lazy version of `orDefault`. Takes a function that returns the default value, that function will be called only if `this` is `Left` */
   orDefaultLazy(getDefaultValue: () => R): R
+  /** Lazy version of `leftOrDefault`. Takes a function that returns the default value, that function will be called only if `this` is `Right` */
   leftOrDefaultLazy(getDefaultValue: () => L): L
+  /** Runs an effect if `this` is `Left`, returns `this` to make chaining other methods possible */
   ifLeft(effect: (value: L) => any): this
+  /** Runs an effect if `this` is `Right`, returns `this` to make chaining other methods possible */
   ifRight(effect: (value: R) => any): this
+  /** Constructs a `Just` with the value of `this` if it\'s `Right` or a `Nothing` if `this` is `Left` */
   toMaybe(): Maybe<R>
+  /** Constructs a `Just` with the value of `this` if it\'s `Left` or a `Nothing` if `this` is `Right` */
   leftToMaybe(): Maybe<L>
+  /** Given two map functions, maps using the first if `this` is `Left` or using the second one if `this` is `Right`.
+   * If you want the functions to return different types depending on the either you may want to use `Either#bimap` instead
+   * */
   either<T>(ifLeft: (value: L) => T, ifRight: (value: R) => T): T
+  /** Extracts the value out of `this` */
   extract(): L | R
 }
 
 interface EitherTypeRef {
+  /** Takes a value and wraps it in a `Right` */
   of<L, R>(value: R): Either<L, R>
+  /** Takes a list of eithers and returns a list of all `Left` values */
   lefts<L, R>(list: Either<L, R>[]): L[]
+  /** Takes a list of eithers and returns a list of all `Right` values */
   rights<L, R>(list: Either<L, R>[]): R[]
+  /** Calls a function and returns a `Right` with the return value or an exception wrapped in a `Left` in case of failure */
   encase<L extends Error, R>(throwsF: () => R): Either<L, R>
 }
 
--- src/Tuple.ts
+++ src/Tuple.ts
@@ -1,5 +1,6 @@
 export interface TupleTypeRef {
   <F, S>(fst: F, snd: S): Tuple<F, S>
+  /** Applies two functions over a single value and constructs a tuple from the results */
   fanout<F, S, T>(f: (value: T) => F, g: (value: T) => S, value: T): Tuple<F, S>
   fanout<F, S, T>(
     f: (value: T) => F,
@@ -8,6 +9,7 @@ export interface TupleTypeRef {
   fanout<F, T>(
     f: (value: T) => F
   ): <S>(g: (value: T) => S) => (value: T) => Tuple<F, S>
+  /** Constructs a tuple from an array with two elements */
   fromArray<F, S>([fst, snd]: [F, S]): Tuple<F, S>
 }
 
@@ -19,15 +21,24 @@ export interface Tuple<F, S> extends Iterable<F | S>, ArrayLike<F | S> {
   toJSON(): [F, S]
   inspect(): string
   toString(): string
+  /** Returns the first value of `this` */
   fst(): F
+  /** Returns the second value of `this` */
   snd(): S
+  /** Compares the values inside `this` and another tuple */
   equals(other: Tuple<F, S>): boolean
+  /** Transforms the two values inside `this` with two mapper functions */
   bimap<F2, S2>(f: (fst: F) => F2, g: (snd: S) => S2): Tuple<F2, S2>
+  /** Applies a function to the first value of `this` */
   mapFirst<F2>(f: (fst: F) => F2): Tuple<F2, S>
+  /** Applies a function to the second value of `this` */
   map<S2>(f: (snd: S) => S2): Tuple<F, S2>
   reduce<T>(reducer: (accumulator: T, value: S) => T, initialValue: T): T
+  /** Returns an array with 2 elements - the values inside `this` */
   toArray(): [F, S]
+  /** Swaps the values inside `this` */
   swap(): Tuple<S, F>
+  /** Applies the second value of a tuple to the second value of `this` */
   ap<T, S2>(f: Tuple<T, (value: S) => S2>): Tuple<F, S2>
 }
 
