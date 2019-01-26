7a3160e65dbd27cd371608ea6ffd3dcf5d30d4fa
gigobyte <gigobest2@gmail.com>
2019-01-26 18:46:50 +0200

    MaybeAsync tests

--- src/MaybeAsync.test.ts
+++ src/MaybeAsync.test.ts
@@ -15,12 +15,51 @@ describe('MaybeAsync', () => {
     })
   })
 
+  test('map', async () => {
+    const newMaybeAsync = MaybeAsync(() => Promise.resolve(5)).map(_ => 'val')
+
+    expect(await newMaybeAsync.run()).toEqualStringified(Just('val'))
+  })
+
+  test('chain', async () => {
+    const newMaybeAsync = MaybeAsync(() => Promise.resolve(5)).chain(_ =>
+      MaybeAsync(() => Promise.resolve('val'))
+    )
+
+    expect(await newMaybeAsync.run()).toEqualStringified(Just('val'))
+  })
+
   describe('run', () => {
     it('resolves to Nothing if any of the async Maybes are Nothing', async () => {
-      const a = await MaybeAsync(({ fromPromise }) =>
-        fromPromise(Promise.resolve(Nothing))
-      ).run()
-      expect(a).toEqual(Nothing)
+      expect(
+        await MaybeAsync(({ fromPromise }) =>
+          fromPromise(Promise.resolve(Nothing))
+        ).run()
+      ).toEqual(Nothing)
+    })
+
+    it('resolves to Nothing if there is a rejected promise', async () => {
+      expect(
+        await MaybeAsync(({ fromPromise }) =>
+          fromPromise(Promise.reject())
+        ).run()
+      ).toEqual(Nothing)
+    })
+
+    it('resolves to Nothing if there is an exception thrown', async () => {
+      expect(
+        await MaybeAsync(() => {
+          throw new Error('!')
+        }).run()
+      ).toEqual(Nothing)
+    })
+
+    it('resolve to Just if the promise resolves successfully', async () => {
+      expect(
+        await MaybeAsync(({ fromPromise }) =>
+          fromPromise(Promise.resolve(Just(5)))
+        ).run()
+      ).toEqualStringified(Just(5))
     })
   })
 })
