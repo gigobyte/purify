06415c27b8fcebf08bf01e5dfd74d1ca7e71ec4e
gigobyte <gigobest2@gmail.com>
2019-11-09 21:18:38 +0200

    Adding basic tests

--- src/Codec.test.ts
+++ src/Codec.test.ts
@@ -1,5 +1,58 @@
 import * as t from 'io-ts'
-import { Codec, number, string, oneOf, GetInterface } from './Codec'
+import { Codec, number, string, oneOf, GetInterface, nullType } from './Codec'
+import { Left, Right } from './Either'
+
+describe('Codec', () => {
+  describe('string', () => {
+    test('decode', () => {
+      expect(string.decode(0)).toEqual(Left('fail'))
+      expect(string.decode({})).toEqual(Left('fail'))
+      expect(string.decode(undefined)).toEqual(Left('fail'))
+      expect(string.decode(null)).toEqual(Left('fail'))
+      expect(string.decode(false)).toEqual(Left('fail'))
+
+      expect(string.decode('')).toEqual(Right(''))
+    })
+
+    test('encode', () => {
+      expect(string.encode('')).toEqual('')
+    })
+  })
+
+  describe('number', () => {
+    test('decode', () => {
+      expect(number.decode('')).toEqual(Left('fail'))
+      expect(number.decode({})).toEqual(Left('fail'))
+      expect(number.decode(undefined)).toEqual(Left('fail'))
+      expect(number.decode(null)).toEqual(Left('fail'))
+      expect(string.decode(false)).toEqual(Left('fail'))
+
+      expect(number.decode(0)).toEqual(Right(0))
+    })
+
+    test('encode', () => {
+      expect(number.encode(0)).toEqual(0)
+    })
+  })
+
+  describe('null', () => {
+    test('decode', () => {
+      expect(nullType.decode('')).toEqual(Left('fail'))
+      expect(nullType.decode({})).toEqual(Left('fail'))
+      expect(nullType.decode(undefined)).toEqual(Left('fail'))
+      expect(nullType.decode(0)).toEqual(Left('fail'))
+      expect(string.decode(false)).toEqual(Left('fail'))
+
+      expect(nullType.decode(null)).toEqual(Right(null))
+    })
+
+    test('encode', () => {
+      expect(nullType.encode(null)).toEqual(null)
+    })
+  })
+})
+
+// ---------------
 
 const User = Codec.interface({
   userId: number,
@@ -19,6 +72,3 @@ type User2 = t.TypeOf<typeof User2>
 
 const u = User.decode({ userId: 2, name: 'dsa', dasda: 5 })
 const u2 = User2.decode({ userId: 2, name: 'dsa', dasda: 5 })
-
-console.log(u)
-console.log(u2)
