f81d68b5538632c6ac433d7788c8a75028171471
gigobyte <gigobest2@gmail.com>
2019-11-10 00:40:24 +0200

    Add a lot of codecs and tests

--- src/Codec.test.ts
+++ src/Codec.test.ts
@@ -1,15 +1,61 @@
-import * as t from 'io-ts'
-import { Codec, number, string, oneOf, GetInterface, nullType } from './Codec'
+import {
+  Codec,
+  number,
+  string,
+  oneOf,
+  GetInterface,
+  nullType,
+  undefinedType,
+  boolean,
+  unknown,
+  array,
+  record,
+  exactly,
+  lazy,
+  maybe,
+  nonEmptyList
+} from './Codec'
 import { Left, Right } from './Either'
+import { Just, Nothing } from './Maybe'
+import { NonEmptyList } from './NonEmptyList'
 
 describe('Codec', () => {
+  describe('interface', () => {
+    const mockCodec = Codec.interface({
+      a: number,
+      b: string
+    })
+
+    test('decode', () => {
+      const failingInputs = [0, {}, undefined, null, false, { a: 0 }, { b: '' }]
+
+      failingInputs.forEach(input => {
+        expect(mockCodec.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(mockCodec.decode({ a: 0, b: '' })).toEqual(Right({ a: 0, b: '' }))
+      expect(mockCodec.decode({ a: 0, b: '', c: '' })).toEqual(
+        Right({ a: 0, b: '' })
+      )
+    })
+
+    test('encode', () => {
+      expect(mockCodec.encode({ a: 0, b: '' })).toEqual({ a: 0, b: '' })
+      expect(mockCodec.encode({ a: 0, b: '', c: '' } as any)).toEqual({
+        a: 0,
+        b: ''
+      })
+      expect(mockCodec.encode({} as any)).toEqual({})
+    })
+  })
+
   describe('string', () => {
     test('decode', () => {
-      expect(string.decode(0)).toEqual(Left('fail'))
-      expect(string.decode({})).toEqual(Left('fail'))
-      expect(string.decode(undefined)).toEqual(Left('fail'))
-      expect(string.decode(null)).toEqual(Left('fail'))
-      expect(string.decode(false)).toEqual(Left('fail'))
+      const failingInputs = [0, {}, undefined, null, false]
+
+      failingInputs.forEach(input => {
+        expect(string.decode(input)).toEqual(Left('fail'))
+      })
 
       expect(string.decode('')).toEqual(Right(''))
     })
@@ -21,11 +67,11 @@ describe('Codec', () => {
 
   describe('number', () => {
     test('decode', () => {
-      expect(number.decode('')).toEqual(Left('fail'))
-      expect(number.decode({})).toEqual(Left('fail'))
-      expect(number.decode(undefined)).toEqual(Left('fail'))
-      expect(number.decode(null)).toEqual(Left('fail'))
-      expect(string.decode(false)).toEqual(Left('fail'))
+      const failingInputs = ['', {}, undefined, null, false]
+
+      failingInputs.forEach(input => {
+        expect(number.decode(input)).toEqual(Left('fail'))
+      })
 
       expect(number.decode(0)).toEqual(Right(0))
     })
@@ -37,11 +83,11 @@ describe('Codec', () => {
 
   describe('null', () => {
     test('decode', () => {
-      expect(nullType.decode('')).toEqual(Left('fail'))
-      expect(nullType.decode({})).toEqual(Left('fail'))
-      expect(nullType.decode(undefined)).toEqual(Left('fail'))
-      expect(nullType.decode(0)).toEqual(Left('fail'))
-      expect(string.decode(false)).toEqual(Left('fail'))
+      const failingInputs = ['', {}, undefined, 0, false]
+
+      failingInputs.forEach(input => {
+        expect(nullType.decode(input)).toEqual(Left('fail'))
+      })
 
       expect(nullType.decode(null)).toEqual(Right(null))
     })
@@ -50,25 +96,227 @@ describe('Codec', () => {
       expect(nullType.encode(null)).toEqual(null)
     })
   })
+
+  describe('undefined', () => {
+    test('decode', () => {
+      const failingInputs = ['', {}, null, 0, false]
+
+      failingInputs.forEach(input => {
+        expect(undefinedType.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(undefinedType.decode(undefined)).toEqual(Right(undefined))
+    })
+
+    test('encode', () => {
+      expect(undefinedType.encode(undefined)).toEqual(undefined)
+    })
+  })
+
+  describe('boolean', () => {
+    test('decode', () => {
+      const failingInputs = ['', {}, null, 0, undefined]
+
+      failingInputs.forEach(input => {
+        expect(boolean.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(boolean.decode(true)).toEqual(Right(true))
+    })
+
+    test('encode', () => {
+      expect(boolean.encode(true)).toEqual(true)
+    })
+  })
+
+  describe('unknown', () => {
+    const inputs = ['', {}, null, 0, undefined, false]
+
+    test('decode', () => {
+      inputs.forEach(input => {
+        expect(unknown.decode(input)).toEqual(Right(input))
+      })
+    })
+
+    test('encode', () => {
+      inputs.forEach(input => {
+        expect(unknown.encode(input)).toEqual(input)
+      })
+    })
+  })
+
+  describe('array', () => {
+    test('decode', () => {
+      const failingInputs = ['', {}, null, 0, undefined, ['']]
+      const numberArray = array(number)
+
+      failingInputs.forEach(input => {
+        expect(numberArray.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(numberArray.decode([])).toEqual(Right([]))
+      expect(numberArray.decode([0])).toEqual(Right([0]))
+    })
+
+    test('encode', () => {
+      const mockCodec = Codec.custom<number>({
+        decode: number.decode,
+        encode: (input: number) => input + 1
+      })
+
+      expect(array(unknown).encode([])).toEqual([])
+      expect(array(mockCodec).encode([1, 2, 3])).toEqual([2, 3, 4])
+    })
+  })
+
+  describe('record', () => {
+    test('decode', () => {
+      const failingInputs = ['', [], null, 0, undefined, { a: true }]
+      const numberRecord = record(string, number)
+
+      failingInputs.forEach(input => {
+        expect(numberRecord.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(numberRecord.decode({})).toEqual(Right({}))
+      expect(numberRecord.decode({ a: 0 })).toEqual(Right({ a: 0 }))
+      expect(numberRecord.decode({ a: 0, b: 1 })).toEqual(Right({ a: 0, b: 1 }))
+    })
+
+    test('decode with number key', () => {
+      const numberRecord = record(number, number)
+
+      expect(numberRecord.decode({ a: 0 })).toEqual(Left('fail'))
+      expect(numberRecord.decode({ 0: 0 })).toEqual(Right({ 0: 0 }))
+    })
+
+    test('encode', () => {
+      const mockKeyCodec = Codec.custom<string>({
+        decode: string.decode,
+        encode: _ => 'haha'
+      })
+
+      const mockValueCodec = Codec.custom<number>({
+        decode: number.decode,
+        encode: (input: number) => input + 1
+      })
+
+      expect(record(string, string).encode({})).toEqual({})
+      expect(record(mockKeyCodec, mockValueCodec).encode({ a: 0 })).toEqual({
+        haha: 1
+      })
+    })
+  })
+
+  describe('oneOf', () => {
+    const mockCodec = oneOf([string, boolean])
+
+    test('decode', () => {
+      const failingInputs = [{}, undefined, 0, []]
+
+      failingInputs.forEach(input => {
+        expect(mockCodec.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(mockCodec.decode('')).toEqual(Right(''))
+      expect(mockCodec.decode(false)).toEqual(Right(false))
+    })
+
+    test('encode', () => {
+      expect(mockCodec.encode('')).toEqual('')
+      expect(mockCodec.encode(false)).toEqual(false)
+
+      const fancyStringCodec = Codec.custom<string>({
+        decode: string.decode,
+        encode: _ => 'always!'
+      })
+
+      const fancyNumberCodec = Codec.custom<number>({
+        decode: number.decode,
+        encode: input => input + 1
+      })
+
+      const fancyMockCodec = oneOf([fancyStringCodec, fancyNumberCodec])
+
+      expect(fancyMockCodec.encode('')).toEqual('always!')
+      expect(fancyMockCodec.encode(1)).toEqual(2)
+      expect(fancyMockCodec.encode(true as any)).toEqual(true)
+    })
+  })
+
+  describe('exactly', () => {
+    test('decode', () => {
+      expect(exactly(0).decode(10)).toEqual(Left('fail'))
+      expect(exactly(0).decode('')).toEqual(Left('fail'))
+      expect(exactly(0).decode(false)).toEqual(Left('fail'))
+
+      expect(exactly(0).decode(0)).toEqual(Right(0))
+
+      expect(exactly('a').decode('b')).toEqual(Left('fail'))
+      expect(exactly('a').decode('')).toEqual(Left('fail'))
+      expect(exactly('a').decode(false)).toEqual(Left('fail'))
+
+      expect(exactly('a').decode('a')).toEqual(Right('a'))
+
+      expect(exactly(true).decode(false)).toEqual(Left('fail'))
+      expect(exactly(true).decode('')).toEqual(Left('fail'))
+      expect(exactly(true).decode(0)).toEqual(Left('fail'))
+
+      expect(exactly(true).decode(true)).toEqual(Right(true))
+    })
+
+    test('encode', () => {
+      expect(exactly(0).encode(0)).toEqual(0)
+      expect(exactly('a').encode('a')).toEqual('a')
+      expect(exactly(true).encode(true)).toEqual(true)
+    })
+  })
+
+  describe('maybe', () => {
+    test('decode', () => {
+      const failingInputs = ['', {}, [], false]
+      const maybeNumber = maybe(number)
+
+      failingInputs.forEach(input => {
+        expect(maybeNumber.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(maybeNumber.decode(0)).toEqual(Right(Just(0)))
+      expect(maybeNumber.decode(null)).toEqual(Right(Nothing))
+      expect(maybeNumber.decode(undefined)).toEqual(Right(Nothing))
+    })
+
+    test('encode', () => {
+      const maybeNumber = maybe(number)
+
+      expect(maybeNumber.encode(Nothing)).toEqual(undefined)
+      expect(maybeNumber.encode(Just(0))).toEqual(0)
+    })
+  })
+
+  describe('nonEmptyList', () => {
+    test('decode', () => {
+      const failingInputs = ['', {}, null, 0, undefined, [''], []]
+      const numberNEL = nonEmptyList(number)
+
+      failingInputs.forEach(input => {
+        expect(numberNEL.decode(input)).toEqual(Left('fail'))
+      })
+
+      expect(numberNEL.decode([0])).toEqual(Right(NonEmptyList([0])))
+    })
+
+    test('encode', () => {
+      const mockCodec = Codec.custom<number>({
+        decode: number.decode,
+        encode: (input: number) => input + 1
+      })
+
+      expect(nonEmptyList(mockCodec).encode(NonEmptyList([1, 2, 3]))).toEqual([
+        2,
+        3,
+        4
+      ])
+    })
+  })
 })
-
-// ---------------
-
-const User = Codec.interface({
-  userId: number,
-  name: string,
-  stupid: oneOf([number, string])
-})
-
-interface User extends GetInterface<typeof User> {}
-
-const User2 = t.type({
-  userId: t.number,
-  name: t.string,
-  stupid: t.union([t.number, t.string])
-})
-
-type User2 = t.TypeOf<typeof User2>
-
-const u = User.decode({ userId: 2, name: 'dsa', dasda: 5 })
-const u2 = User2.decode({ userId: 2, name: 'dsa', dasda: 5 })
--- src/Codec.ts
+++ src/Codec.ts
@@ -1,5 +1,7 @@
 import { Either, Right, Left } from './Either'
 import { identity } from './Function'
+import { Maybe, Just, Nothing } from './Maybe'
+import { NonEmptyList } from './NonEmptyList'
 
 export interface Codec<T> {
   decode: (input: unknown) => Either<string, T>
@@ -9,6 +11,9 @@ export interface Codec<T> {
 export type GetInterface<T extends Codec<any>> = T extends Codec<infer U>
   ? U
   : never
+
+const isObject = (obj: unknown): obj is object =>
+  typeof obj === 'object' && obj !== null && !Array.isArray(obj)
 
 export const Codec = {
   interface<T extends Record<string, Codec<any>>>(
@@ -20,11 +25,15 @@ export const Codec = {
   > {
     return {
       decode: (input: any) => {
+        if (!isObject(input)) {
+          return Left('fail')
+        }
+
         const result = {} as { [k in keyof T]: GetInterface<T[k]> }
         const keys = Object.keys(properties)
 
         for (const key of keys) {
-          const decodedProperty = properties[key].decode(input[key])
+          const decodedProperty = properties[key].decode((input as any)[key])
 
           if (decodedProperty.isLeft()) {
             return Left('fail')
@@ -86,6 +95,16 @@ export const undefinedType = Codec.custom<undefined>({
   encode: identity
 })
 
+export const boolean = Codec.custom<boolean>({
+  decode: input => (typeof input === 'boolean' ? Right(input) : Left('fail')),
+  encode: identity
+})
+
+export const unknown = Codec.custom<unknown>({
+  decode: Right,
+  encode: identity
+})
+
 export const oneOf = <T extends Array<Codec<any>>>(
   codecs: T
 ): Codec<GetInterface<T extends Array<infer U> ? U : never>> =>
@@ -111,3 +130,111 @@ export const oneOf = <T extends Array<Codec<any>>>(
       return input
     }
   })
+
+export const array = <T>(codec: Codec<T>): Codec<Array<T>> =>
+  Codec.custom({
+    decode: input => {
+      if (!Array.isArray(input)) {
+        return Left('fail')
+      } else {
+        const result: T[] = []
+
+        for (const e of input) {
+          const decoded = codec.decode(e)
+
+          if (decoded.isRight()) {
+            result.push(decoded.extract())
+          } else {
+            return Left('fail')
+          }
+        }
+
+        return Right(result)
+      }
+    },
+    encode: input => input.map(codec.encode)
+  })
+
+const numberString = Codec.custom<any>({
+  decode: input =>
+    string.decode(input).chain(x => (isFinite(+x) ? Right(x) : Left('fail'))),
+  encode: identity
+})
+
+export const record = <K extends keyof any, V>(
+  keyCodec: Codec<K>,
+  valueCodec: Codec<V>
+): Codec<Record<K, V>> =>
+  Codec.custom({
+    decode: input => {
+      const result = {} as Record<K, V>
+      const keyCodecOverride: Codec<K> =
+        (keyCodec as any) === number ? numberString : keyCodec
+
+      if (!isObject(input)) {
+        return Left('fail')
+      }
+
+      for (const key of Object.keys(input)) {
+        if (input.hasOwnProperty(key)) {
+          const decodedKey = keyCodecOverride.decode(key)
+          const decodedValue = valueCodec.decode((input as any)[key])
+
+          if (decodedKey.isRight() && decodedValue.isRight()) {
+            result[decodedKey.extract()] = decodedValue.extract()
+          } else {
+            return Left('fail')
+          }
+        }
+      }
+
+      return Right(result)
+    },
+    encode: input => {
+      const result = {} as Record<K, V>
+
+      for (const key in input) {
+        if (input.hasOwnProperty(key)) {
+          result[keyCodec.encode(key)] = valueCodec.encode(input[key])
+        }
+      }
+
+      return result
+    }
+  })
+
+export const exactly = <T extends string | number | boolean>(
+  expectedValue: T
+): Codec<T> =>
+  Codec.custom({
+    decode: input =>
+      input === expectedValue ? Right(expectedValue) : Left('fail'),
+    encode: identity
+  })
+
+export const lazy = <T>(getCodec: () => Codec<T>): Codec<T> =>
+  Codec.custom({
+    decode: input => getCodec().decode(input),
+    encode: input => getCodec().encode(input)
+  })
+
+export const maybe = <T>(codec: Codec<T>): Codec<Maybe<T>> =>
+  Codec.custom({
+    decode: input =>
+      Maybe.fromNullable(input).caseOf({
+        Just: x => codec.decode(x).map(Just),
+        Nothing: () => Right(Nothing)
+      }),
+    encode: input => input.toJSON()
+  })
+
+export const nonEmptyList = <T>(codec: Codec<T>): Codec<NonEmptyList<T>> => {
+  const arrayCodec = array(codec)
+  return Codec.custom({
+    decode: input =>
+      arrayCodec
+        .decode(input)
+        .chain(x => NonEmptyList.fromArray(x).toEither('fail')),
+    encode: arrayCodec.encode
+  })
+}
