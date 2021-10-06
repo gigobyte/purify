13a17459ccd50cda2236f3623af62170cdf7a19b
gigobyte <gigobest2@gmail.com>
2021-10-06 21:54:22 +0300

    Add v1.1.0 to site

--- site/src/pages/changelog.js
+++ site/src/pages/changelog.js
@@ -52,6 +52,16 @@ const Changelog = (props) => (
     <div>
       <h1>Choose version:</h1>
       <VersionContainer>
+        <VersionStatic>1.1.0</VersionStatic>
+        <Description>
+          Check out the release on{' '}
+          <a href="https://github.com/gigobyte/purify/releases/tag/v1.1.0">
+            GitHub
+          </a>
+        </Description>
+        <Date>October 2021</Date>
+      </VersionContainer>
+      <VersionContainer>
         <VersionStatic>1.0.0</VersionStatic>
         <Description>
           Check out the release on{' '}
