b5ba4122691dbdf32141bed66041feb947aa89e2
gigobyte <gigobest2@gmail.com>
2024-01-01 15:24:49 +0200

    Fix esm build (v2.0.3)

--- site/src/components/Sidebar.js
+++ site/src/components/Sidebar.js
@@ -54,7 +54,7 @@ class Sidebar extends React.Component {
       <Container>
         <Header>
           <HeaderTitle to="/">Purify</HeaderTitle>
-          <HeaderTitleVersion>v2.0.2</HeaderTitleVersion>
+          <HeaderTitleVersion>v2.0.3</HeaderTitleVersion>
           <HamburgerMenu
             onClick={this.toggleMenu}
             opened={this.state.isMenuShown}
